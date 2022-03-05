f97d1fc77e1f693098c5d2bc94d9e4f4c6451e26
Luiz Pavanello <luizpavanello@gmail.com>
2022-03-05 17:49:46 -0300

    Finished the 01_Day and the exercises

---
+++ 01_Day_Introduction/euclidian_distance.py
@@ -0,0 +1,10 @@
+# Python Euclidian Distance using math.dist
+
+from math import dist 
+
+point_1 = (2, 3)
+point_2 = (10, 8)
+
+print(dist(point_1, point_2))
+
+# Result: 9.433981132056603
---
+++ 01_Day_Introduction/helloworld.py
@@ -0,0 +1,23 @@
+# Introduction
+# Day 1 - 30DaysOfPython Challenge
+
+print("Hello World!")
+print(3 + 5) # addition(+)
+print(5 - 3) # subtraction(-)
+print(3 * 5) # multiplication(*)
+print(5 / 3) # division(/)
+print(5 ** 2) # exponentiation(**)
+print(5 % 3) # modulus(%)
+print(5 // 3) # floor division(//)
+
+# Checking data types
+
+print(type(3)) # int
+print(type(3.14)) # float
+print(type(1 + 8j)) # complex
+print(type("Hello World!")) # str
+print(type([1, 2, 3])) # list
+print(type({1: "one", 2: "two", 3: "three"})) # dict
+print(type({1, 2, 3})) # set
+print(type((1, 2, 3))) # tuple
+
